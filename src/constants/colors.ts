a32fc1a6aa0527341875e48cceecbc7cd40638ee
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-13 16:06:44 -0500

    Declared zustand persistent storage. Creathed theme store. Created colors and dark-colors json files. Modified App file.

---
+++ src/constants/colors.ts
@@ -0,0 +1,5 @@
+import * as lightColors from '../../assets/colors.json';
+import * as darkColors from '../../assets/dark-colors.json';
+
+export const LIGHT_COLORS = lightColors;
+export const DARK_COLORS = darkColors;
---
+++ src/stores/theme.store.ts
@@ -0,0 +1,16 @@
+import {persist} from 'zustand/middleware';
+import {zustandPersistentStorage} from '../lib/zustand';
+import {create} from 'zustand';
+import {Appearance} from 'react-native';
+
+interface ThemeState {
+  isDarkMode: boolean;
+}
+
+const useThemeStore = create<ThemeState>()(
+  persist(() => ({isDarkMode: Appearance.getColorScheme() === 'dark'}), {
+    name: 'theme-state',
+    storage: zustandPersistentStorage,
+  }),
+);
+export const useIsDarkMode = () => useThemeStore(state => state.isDarkMode);
