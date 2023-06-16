5a206fd05708cc9a39362eda9aa0c40e11cfa76c
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-16 17:35:00 -0500

    Added status bar component.

--- src/app/providers/react-native-paper.provider.tsx
+++ src/app/providers/react-native-paper.provider.tsx
@@ -2,10 +2,13 @@ import {ReactNode} from 'react';
 import {PaperProvider} from 'react-native-paper';
 import {useIsDarkMode} from '../../stores/theme.store';
 import {DARK_THEME, LIGHT_THEME} from '../../constants/theme';
+import {StatusBar} from 'react-native';
+import {DARK_COLORS, LIGHT_COLORS} from '../../constants/colors';
 
 interface ReactNativePaperProviderProps {
   children?: ReactNode;
 }
+
 export const ReactNativePaperProvider = (
   props: ReactNativePaperProviderProps,
 ) => {
@@ -13,6 +16,12 @@ export const ReactNativePaperProvider = (
 
   return (
     <PaperProvider theme={isDarkMode ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar
+        backgroundColor={
+          isDarkMode ? DARK_COLORS.primary : LIGHT_COLORS.primary
+        }
+        barStyle={isDarkMode ? 'dark-content' : 'light-content'}
+      />
       {props.children}
     </PaperProvider>
   );
