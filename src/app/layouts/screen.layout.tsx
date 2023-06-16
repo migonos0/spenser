45387a3c49c33d79851051cfd824f33126bc60e5
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-16 17:28:49 -0500

    Declared twClass as prop. Replaced View component in favor of Surface component.

--- src/app/layouts/screen.layout.tsx
+++ src/app/layouts/screen.layout.tsx
@@ -1,10 +1,14 @@
+import classNames from 'classnames';
 import {ReactNode} from 'react';
-import {View} from 'react-native';
+import {Surface} from 'react-native-paper';
 
 interface ScreenLayoutProps {
   children?: ReactNode;
+  twClass?: string;
 }
 
 export const ScreenLayout = (props: ScreenLayoutProps) => (
-  <View>{props.children}</View>
+  <Surface className={classNames('h-full', props.twClass)}>
+    {props.children}
+  </Surface>
 );
