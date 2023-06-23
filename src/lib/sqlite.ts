ef33f60fc100be37d49bf7a41f80d38113f48894
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-22 22:12:59 -0500

    Implemented getInsertId function.

--- src/lib/sqlite.ts
+++ src/lib/sqlite.ts
@@ -1,4 +1,15 @@
-import {openDatabase} from 'react-native-sqlite-storage';
+import {ResultSet, openDatabase} from 'react-native-sqlite-storage';
 
 export const getSQLiteDatabase = (dbName: string) =>
   openDatabase({name: dbName, location: 'default'});
+
+export const getInsertId = (result: [ResultSet]) => {
+  if (result.length < 1) {
+    throw new Error('Result set is empty.');
+  }
+  if (result[0].rowsAffected < 1) {
+    throw new Error('No rows were affected.');
+  }
+
+  return result[0].insertId;
+};
