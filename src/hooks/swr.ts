7cf730b764752ac1705a28c29024be0009707d38
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-22 22:12:17 -0500

    Declared useSWRMutation alias. Implemented useSWRMutation custom hook.

--- src/hooks/swr.ts
+++ src/hooks/swr.ts
@@ -2,6 +2,7 @@ import swrImmutableHook from 'swr/immutable';
 import swrHook, {Key} from 'swr';
 import {SQLiteDatabase} from 'react-native-sqlite-storage';
 import {useAreTablesCreated, useSQLiteDatabase} from '../state/sqlite.state';
+import swrMutationHook from 'swr/mutation';
 
 export const useSWRImmutable = swrImmutableHook;
 export const useSWR = swrHook;
@@ -16,4 +17,26 @@ export const useSWRSQLite = <Data>(
   const fetcher2 = () => sqliteDatabase && fetcher(sqliteDatabase);
 
   return useSWRImmutable(key2, fetcher2);
+};
+export const useSWRMutation = swrMutationHook;
+export const useSWRSQLiteMutation = <T, U, V = any>(
+  key: Key,
+  fetcher: (input: T) => (sqliteDatabase: SQLiteDatabase) => U | Promise<U>,
+  populateCache?: (result: U | undefined, currentData: V) => V,
+) => {
+  const sqliteDatabase = useSQLiteDatabase();
+  const areTablesCreated = useAreTablesCreated();
+
+  const key2 = areTablesCreated ? key : undefined;
+  const fetcher2 = async (_: unknown, {arg}: {arg: T}) => {
+    if (!sqliteDatabase) {
+      return;
+    }
+    return await fetcher(arg)(sqliteDatabase);
+  };
+
+  return useSWRMutation(key2, fetcher2, {
+    revalidate: false,
+    populateCache: populateCache,
+  });
 };
