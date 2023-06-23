33e12b7f3429f8050298f63d9fd83003cdef4a6a
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-22 20:42:24 -0500

    Consumed useMessages state hook.

--- src/app/screens/chat.screen.tsx
+++ src/app/screens/chat.screen.tsx
@@ -1,10 +1,22 @@
-import {Text} from 'react-native-paper';
 import {ScreenLayout} from '../layouts/screen.layout';
+import {useAppTheme} from '../../hooks/use-app-theme';
+import {Card, Text} from 'react-native-paper';
+import {useMessages} from '../../state/message.state';
 
 export const ChatScreen = () => {
+  const {colors} = useAppTheme();
+  const {messages} = useMessages();
+
   return (
-    <ScreenLayout>
-      <Text>Chat Screen</Text>
+    <ScreenLayout colors={colors}>
+      <Card>
+        <Card.Content>
+          {messages?.map((message, index) => (
+            <Text key={index}>{message.description}</Text>
+          ))}
+          <Text>Me</Text>
+        </Card.Content>
+      </Card>
     </ScreenLayout>
   );
 };
