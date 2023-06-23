1ebc698e61bd7e13a49038e061c77367b9d35fea
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-23 17:59:14 -0500

    Modified aliases in order for them to log errors when they happen.

--- src/hooks/swr.ts
+++ src/hooks/swr.ts
@@ -1,11 +1,30 @@
 import swrImmutableHook from 'swr/immutable';
-import swrHook, {Key} from 'swr';
+import swrHook, {BareFetcher, Key, SWRConfiguration} from 'swr';
 import {SQLiteDatabase} from 'react-native-sqlite-storage';
 import {useAreTablesCreated, useSQLiteDatabase} from '../state/sqlite.state';
-import swrMutationHook from 'swr/mutation';
+import swrMutationHook, {
+  MutationFetcher,
+  SWRMutationConfiguration,
+} from 'swr/mutation';
 
-export const useSWRImmutable = swrImmutableHook;
-export const useSWR = swrHook;
+export const useSWRImmutable = <Data>(
+  key: Key,
+  fetcher: BareFetcher<Data>,
+  config?: SWRConfiguration<Data, Error, BareFetcher<Data>>,
+) => {
+  const result = swrImmutableHook(key, fetcher, config);
+  result.error && console.error(result.error);
+  return result;
+};
+export const useSWR = <Data>(
+  key: Key,
+  fetcher: BareFetcher<Data>,
+  config?: SWRConfiguration<Data, Error, BareFetcher<Data>>,
+) => {
+  const result = swrHook(key, fetcher, config);
+  result.error && console.log(result.error);
+  return result;
+};
 export const useSWRSQLite = <Data>(
   key: Key,
   fetcher: (sqliteDatabase: SQLiteDatabase) => Data | Promise<Data>,
@@ -18,7 +37,15 @@ export const useSWRSQLite = <Data>(
 
   return useSWRImmutable(key2, fetcher2);
 };
-export const useSWRMutation = swrMutationHook;
+export const useSWRMutation = <Data, ExtraArg = never>(
+  key: Key,
+  fetcher: MutationFetcher<Data, ExtraArg, Key>,
+  options: SWRMutationConfiguration<Data, Error, ExtraArg, Key>,
+) => {
+  const result = swrMutationHook(key, fetcher, options);
+  result.error && console.error(result.error);
+  return result;
+};
 export const useSWRSQLiteMutation = <T, U, V = any>(
   key: Key,
   fetcher: (input: T) => (sqliteDatabase: SQLiteDatabase) => U | Promise<U>,
