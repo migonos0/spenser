25752fcd99d3adc2124326afe25bd20191d1c526
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-26 20:39:32 -0500

    Removed comments. Added onSendButtonPress callback. Added avoidCleaningAfterEffect prop. Implementend functionallity to automatically clean the input after it's been sent.

--- src/app/components/chat-box.tsx
+++ src/app/components/chat-box.tsx
@@ -1,21 +1,37 @@
 import classNames from 'classnames';
 import {View} from 'react-native';
 import {Button, TextInput} from 'react-native-paper';
+import {useState} from 'react';
+import {LOCALE} from '../../constants/locale';
 
 interface ChatBoxProps {
   twClass?: string;
+  onSendButtonPress?: (t: string) => void;
+  avoidCleaningAfterSent?: boolean;
 }
 export const ChatBox = (props: ChatBoxProps) => {
+  const [text, setText] = useState<string>('');
+
   return (
     <View
       className={classNames('flex flex-row w-full px-5 py-3', props.twClass)}>
-      {/* <TextInput className="flex-1 mr-2 bg-blue-500" mode="outlined" /> */}
       <View className="flex-1 flex justify-center content-center pb-2 mr-2">
-        <TextInput mode="outlined" />
+        <TextInput
+          multiline
+          value={text}
+          onChangeText={t => setText(t)}
+          mode="outlined"
+        />
       </View>
       <View className="flex justify-center">
-        <Button icon={'send'} mode="contained">
-          Send
+        <Button
+          onPress={() => {
+            props.onSendButtonPress && props.onSendButtonPress(text);
+            !props.avoidCleaningAfterSent && setText('');
+          }}
+          icon={'send'}
+          mode="contained">
+          {LOCALE.common.send}
         </Button>
       </View>
     </View>
