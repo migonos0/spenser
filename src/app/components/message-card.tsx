81f470f702f7dadb5c5357c23d62bce409dd93b9
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-26 20:34:43 -0500

    Created message card component.

---
+++ src/app/components/message-card.tsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import {View} from 'react-native';
+import {Card, Text} from 'react-native-paper';
+
+interface MessageCardProps {
+  cardTitle?: string;
+  body?: string;
+  twClass?: string;
+  isNotSentByTheUser?: boolean;
+  backgroundColor?: string;
+}
+
+export const MessageCard = (props: MessageCardProps) => {
+  return (
+    <View
+      className={classNames(
+        {'pr-8': props.isNotSentByTheUser, 'pl-8': !props.isNotSentByTheUser},
+        props.twClass,
+      )}>
+      <Card style={{backgroundColor: props.backgroundColor}}>
+        {props.cardTitle && (
+          <Card.Content>
+            <Text variant="titleMedium">{props.cardTitle}</Text>
+          </Card.Content>
+        )}
+        <Card.Content>
+          <Text variant="bodyMedium">{props.body}</Text>
+        </Card.Content>
+      </Card>
+    </View>
+  );
+};
