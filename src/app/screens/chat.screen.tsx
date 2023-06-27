fdae086186b2cbe7298d53cd9a6d313d6056f33a
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-27 15:14:00 -0500

    Implemented base chat screen.

--- src/app/screens/chat.screen.tsx
+++ src/app/screens/chat.screen.tsx
@@ -1,22 +1,41 @@
 import {ScreenLayout} from '../layouts/screen.layout';
 import {useAppTheme} from '../../hooks/use-app-theme';
-import {Card, Text} from 'react-native-paper';
-import {useMessages} from '../../state/message.state';
+import {useMessages, useMutateMessages} from '../../state/message.state';
+import {ChatBox} from '../components/chat-box';
+import {FlatList} from 'react-native';
+import {MessageCard} from '../components/message-card';
 
 export const ChatScreen = () => {
   const {colors} = useAppTheme();
   const {messages} = useMessages();
+  const {trigger} = useMutateMessages();
+
+  const onSendButtonPress = (text: string) => {
+    trigger({amount: 1, description: text, isIncome: true});
+  };
 
   return (
-    <ScreenLayout colors={colors}>
-      <Card>
-        <Card.Content>
-          {messages?.map((message, index) => (
-            <Text key={index}>{message.description}</Text>
-          ))}
-          <Text>Me</Text>
-        </Card.Content>
-      </Card>
+    <ScreenLayout
+      footer={<ChatBox onSendButtonPress={onSendButtonPress} />}
+      colors={colors}>
+      <FlatList
+        inverted
+        className="px-4"
+        data={messages}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item: message}) => (
+          <MessageCard
+            twClass="m-2"
+            backgroundColor={
+              message.isIncome
+                ? colors.surfaceVariant
+                : colors.tertiaryContainer
+            }
+            cardTitle={message.amount.toString()}
+            body={message.description}
+          />
+        )}
+      />
     </ScreenLayout>
   );
 };
