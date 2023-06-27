a9acce138eff07c0864c790e86c0fa6a02134633
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-06-26 20:37:55 -0500

    Modified useMessages and useMutateMessages state functions to behave in accordance with the form of order.

--- src/state/message.state.ts
+++ src/state/message.state.ts
@@ -1,21 +1,26 @@
+import {MESSAGES_TABLE_NAME} from '../constants/db';
 import {useSWRSQLite, useSWRSQLiteMutation} from '../hooks/swr';
 import {createMessage, findAllMessages} from '../service/message.service';
 
-export const useMessages = () => {
-  const {data, mutate} = useSWRSQLite('messages', findAllMessages);
+export const useMessages = (params?: {ascendant?: boolean}) => {
+  const fetcher = findAllMessages({ascendant: params?.ascendant});
+  const {data, mutate} = useSWRSQLite(MESSAGES_TABLE_NAME, fetcher);
 
   return {messages: data, mutateMessages: mutate};
 };
 
-export const useMutateMessages = () => {
+export const useMutateMessages = (params?: {ascendant?: boolean}) => {
   const {trigger} = useSWRSQLiteMutation(
-    'messages',
+    MESSAGES_TABLE_NAME,
     createMessage,
     (result, currentData) => {
       if (!result) {
         return currentData;
       }
-      return [...(currentData ?? []), ...[result]];
+      if (params?.ascendant) {
+        return [...(currentData ?? []), ...[result]];
+      }
+      return [...[result], ...(currentData ?? [])];
     },
   );
 
