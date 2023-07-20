c234be7094bbe165c3953d09c99d8890ba11ba7d
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-07-20 11:13:54 -0500

    Added wereRowsAffected function. Modified getInserId function.

--- src/lib/sqlite.ts
+++ src/lib/sqlite.ts
@@ -3,12 +3,20 @@ import {ResultSet, openDatabase} from 'react-native-sqlite-storage';
 export const getSQLiteDatabase = (dbName: string) =>
   openDatabase({name: dbName, location: 'default'});
 
-export const getInsertId = (result: [ResultSet]) => {
+export const wereRowsAffected = (result: [ResultSet]) => {
   if (result.length < 1) {
     throw new Error('Result set is empty.');
   }
   if (result[0].rowsAffected < 1) {
     throw new Error('No rows were affected.');
+  }
+
+  return true;
+};
+
+export const getInsertId = (result: [ResultSet]) => {
+  if (!wereRowsAffected(result)) {
+    return;
   }
 
   return result[0].insertId;
