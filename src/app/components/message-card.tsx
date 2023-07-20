f248631f9182b3fd23804479ece086dddb6f6e95
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-07-20 10:15:29 -0500

    Added embed dialog that shows actions for long press events. Declared structur for longPressDialog items.

--- src/app/components/message-card.tsx
+++ src/app/components/message-card.tsx
@@ -1,8 +1,9 @@
 import classNames from 'classnames';
-import {View} from 'react-native';
-import {Card, Text} from 'react-native-paper';
+import {StyleProp, TextStyle, View} from 'react-native';
+import {Card, Dialog, List, Portal, Text} from 'react-native-paper';
 import {trigger} from 'react-native-haptic-feedback';
-import {useCallback} from 'react';
+import {useCallback, useState} from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface MessageCardProps {
   cardTitle?: string;
@@ -10,40 +11,79 @@ interface MessageCardProps {
   twClass?: string;
   isNotSentByTheUser?: boolean;
   backgroundColor?: string;
-  onLongPress?: () => void;
+  longPressDialogItems?: {
+    title: string;
+    iconName?: string;
+    onPress: () => void;
+  }[];
 }
 
 export const MessageCard = (props: MessageCardProps) => {
-  const {onLongPress} = props;
+  const [
+    isMessageCardLongPressDialogVisible,
+    setIsMessageCardLongPressDialogVisible,
+  ] = useState(false);
 
   const localOnLongPress = useCallback(() => {
-    if (!onLongPress) {
+    if (!props.longPressDialogItems) {
       return;
     }
     trigger('impactLight');
-    onLongPress();
-  }, [onLongPress]);
+    setIsMessageCardLongPressDialogVisible(true);
+  }, [props.longPressDialogItems]);
+  const onMessageCardLongPressDialogDismiss = useCallback(() => {
+    setIsMessageCardLongPressDialogVisible(false);
+  }, []);
+
+  const longPressMessageIconItemRenderer = (
+    props2: {
+      color: string;
+      style: StyleProp<TextStyle>;
+    },
+    iconName?: string,
+  ) => iconName && <Icon size={24} name={iconName} {...props2} />;
 
   return (
-    <View
-      className={classNames(
-        {'pr-8': props.isNotSentByTheUser, 'pl-8': !props.isNotSentByTheUser},
-        props.twClass,
-      )}>
-      <Card
-        onLongPress={localOnLongPress}
-        style={{backgroundColor: props.backgroundColor}}>
-        {props.cardTitle && (
-          <Card.Content className={classNames({'mb-4': !props.body})}>
-            <Text variant="titleMedium">{props.cardTitle}</Text>
-          </Card.Content>
-        )}
-        {props.body && (
-          <Card.Content>
-            <Text variant="bodyMedium">{props.body}</Text>
-          </Card.Content>
-        )}
-      </Card>
-    </View>
+    <>
+      <View
+        className={classNames(
+          {'pr-8': props.isNotSentByTheUser, 'pl-8': !props.isNotSentByTheUser},
+          props.twClass,
+        )}>
+        <Card
+          onLongPress={localOnLongPress}
+          style={{backgroundColor: props.backgroundColor}}>
+          {props.cardTitle && (
+            <Card.Content className={classNames({'mb-4': !props.body})}>
+              <Text variant="titleMedium">{props.cardTitle}</Text>
+            </Card.Content>
+          )}
+          {props.body && (
+            <Card.Content>
+              <Text variant="bodyMedium">{props.body}</Text>
+            </Card.Content>
+          )}
+        </Card>
+      </View>
+      {props.longPressDialogItems && (
+        <Portal>
+          <Dialog
+            onDismiss={onMessageCardLongPressDialogDismiss}
+            visible={isMessageCardLongPressDialogVisible}>
+            <Dialog.Content>
+              {props.longPressDialogItems.map(item => (
+                <List.Item
+                  onPress={item.onPress}
+                  title={item.title}
+                  left={props2 =>
+                    longPressMessageIconItemRenderer(props2, item.iconName)
+                  }
+                />
+              ))}
+            </Dialog.Content>
+          </Dialog>
+        </Portal>
+      )}
+    </>
   );
 };
