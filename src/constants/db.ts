b1ea178a8d56111a5b78a533b5f60144373ba49e
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-07-20 10:54:18 -0500

    Declared TAGS_TABLE_NAME and TAGS_TABLE_NAME constants.

--- src/constants/db.ts
+++ src/constants/db.ts
@@ -1,2 +1,4 @@
 export const DB_NAME = 'spencer-data.db';
 export const MESSAGES_TABLE_NAME = 'messages';
+export const TAGS_TABLE_NAME = 'tags';
+export const MESSAGES_TAGS_TABLE_NAME = 'messages_tags';
