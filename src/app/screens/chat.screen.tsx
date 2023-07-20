afe35aea206c1329f51a7184b62600931c87363e
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-07-20 10:18:19 -0500

    Replaced useMutateMessages in favor of useCreateMessage. Added useDeleteMessage to longPressDialogItems so the user is able to delete its previous messages.

--- src/app/screens/chat.screen.tsx
+++ src/app/screens/chat.screen.tsx
@@ -3,7 +3,8 @@ import {useAppTheme} from '../../hooks/use-app-theme';
 import {
   useMessageAmountSummatory,
   useMessages,
-  useMutateMessages,
+  useCreateMessage,
+  useDeleteMessage,
 } from '../../state/message.state';
 import {ChatBox} from '../components/chat-box';
 import {FlatList} from 'react-native';
@@ -16,12 +17,14 @@ import {
 } from '../../utilities/message-pattern-finders';
 import {DEVELOPER_MENU_ITEMS} from '../../constants/menu-items';
 import {NODE_ENV} from '../../constants/environment';
+import {LOCALE} from '../../constants/locale';
 
 export const ChatScreen = () => {
   const {colors} = useAppTheme();
   const {messages} = useMessages();
-  const {trigger} = useMutateMessages();
+  const {trigger: triggerMessageCreation} = useCreateMessage();
   const {messageAmountSummatory, updateWithValue} = useMessageAmountSummatory();
+  const {trigger: triggerMessageDeletion} = useDeleteMessage();
 
   const onSendButtonPress = (message: string) => {
     const isExpense = validateExpense(message);
@@ -35,7 +38,7 @@ export const ChatScreen = () => {
     );
     const amount = Number(stringifiedAmount) * (isExpense ? -1 : 1);
 
-    trigger({
+    triggerMessageCreation({
       amount,
       description,
       isExpense,
@@ -62,6 +65,16 @@ export const ChatScreen = () => {
         keyExtractor={item => item.id.toString()}
         renderItem={({item: message}) => (
           <MessageCard
+            longPressDialogItems={[
+              {
+                title: LOCALE.common.delete,
+                iconName: 'delete',
+                onPress: () => {
+                  triggerMessageDeletion(message.id);
+                  updateWithValue(message.amount * -1);
+                },
+              },
+            ]}
             twClass="m-2"
             backgroundColor={
               message.isExpense
