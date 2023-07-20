e73bfe9b041589e60599ae7e6ccc1db626b3d8d6
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-07-20 09:25:55 -0500

    Declared onLongPress function under MessageCard component.

--- src/app/components/message-card.tsx
+++ src/app/components/message-card.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames';
 import {View} from 'react-native';
 import {Card, Text} from 'react-native-paper';
+import {trigger} from 'react-native-haptic-feedback';
+import {useCallback} from 'react';
 
 interface MessageCardProps {
   cardTitle?: string;
@@ -8,16 +10,29 @@ interface MessageCardProps {
   twClass?: string;
   isNotSentByTheUser?: boolean;
   backgroundColor?: string;
+  onLongPress?: () => void;
 }
 
 export const MessageCard = (props: MessageCardProps) => {
+  const {onLongPress} = props;
+
+  const localOnLongPress = useCallback(() => {
+    if (!onLongPress) {
+      return;
+    }
+    trigger('impactLight');
+    onLongPress();
+  }, [onLongPress]);
+
   return (
     <View
       className={classNames(
         {'pr-8': props.isNotSentByTheUser, 'pl-8': !props.isNotSentByTheUser},
         props.twClass,
       )}>
-      <Card style={{backgroundColor: props.backgroundColor}}>
+      <Card
+        onLongPress={localOnLongPress}
+        style={{backgroundColor: props.backgroundColor}}>
         {props.cardTitle && (
           <Card.Content className={classNames({'mb-4': !props.body})}>
             <Text variant="titleMedium">{props.cardTitle}</Text>
