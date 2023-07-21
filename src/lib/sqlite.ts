d0d17c3d081940e53b5d2316d6c6b8a1b085bb8c
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-07-21 11:16:44 -0500

    Modified wereRowsAffectedFunction by replacing it with validateRowAffectation.

--- src/lib/sqlite.ts
+++ src/lib/sqlite.ts
@@ -3,21 +3,17 @@ import {ResultSet, openDatabase} from 'react-native-sqlite-storage';
 export const getSQLiteDatabase = (dbName: string) =>
   openDatabase({name: dbName, location: 'default'});
 
-export const wereRowsAffected = (result: [ResultSet]) => {
+export const validateRowAffectation = (result: [ResultSet]) => {
   if (result.length < 1) {
     throw new Error('Result set is empty.');
   }
   if (result[0].rowsAffected < 1) {
     throw new Error('No rows were affected.');
   }
-
-  return true;
 };
 
 export const getInsertId = (result: [ResultSet]) => {
-  if (!wereRowsAffected(result)) {
-    return;
-  }
+  validateRowAffectation(result);
 
   return result[0].insertId;
 };
