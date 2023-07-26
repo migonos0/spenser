6e018ff799ec55c28e052e7a92c902a24b1473d0
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-07-26 08:07:03 -0500

    Added functions to drop tags table.

--- src/constants/menu-items.ts
+++ src/constants/menu-items.ts
@@ -1,12 +1,18 @@
 import {MenuItem} from '../app/components/app-bar';
+import {dropMessagesTagsTable} from '../service/message-tag.service';
 import {dropMessagesTable} from '../service/message.service';
+import {dropTagsTable} from '../service/tag.service';
 import {sqliteStoreActions} from '../stores/sqlite.store';
 
 export const DEVELOPER_MENU_ITEMS: MenuItem[] = [
   {
     label: 'Drop tables',
     onTouch() {
-      sqliteStoreActions.setWithSqliteDatabase(db => dropMessagesTable(db));
+      sqliteStoreActions.setWithSqliteDatabase(db => {
+        dropMessagesTagsTable(db);
+        dropTagsTable(db);
+        dropMessagesTable(db);
+      });
     },
   },
 ];
