ec97c66b3bf9d43666307e7d18dc01eed7d01a56
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-03 10:11:36 -0500

    Replaced usage of hooks that handle messages individually in favor of hooks that handle messages and tags.

--- src/app/screens/chat.screen.tsx
+++ src/app/screens/chat.screen.tsx
@@ -2,92 +2,60 @@ import {ScreenLayout} from '../layouts/screen.layout';
 import {useAppTheme} from '../../hooks/use-app-theme';
 import {
   useMessageAmountSummatory,
-  useCreateMessage,
-  useDeleteMessage,
   useMessagesWithTags,
+  useCreateMessageWithTags,
+  useDeleteMessageWithTags,
 } from '../../state/message.state';
 import {ChatBox} from '../components/chat-box';
 import {FlatList} from 'react-native';
 import {MessageCard} from '../components/message-card';
 import {
-  cleanMessageDescription,
-  findAmount,
   findTags,
-  validateExpense,
+  getCreatableMessageFromString,
 } from '../../utilities/message-pattern-finders';
 import {DEVELOPER_MENU_ITEMS} from '../../constants/menu-items';
 import {NODE_ENV} from '../../constants/environment';
 import {LOCALE} from '../../constants/locale';
 import {useCreateTags, useTags} from '../../state/tag.state';
-import {useCreateMessagesTags} from '../../state/message-tag.state';
+import {MessageWithTags} from '../../schemas/message.schema';
 
 export const ChatScreen = () => {
   const {colors} = useAppTheme();
-  const {trigger: triggerMessageCreation} = useCreateMessage();
   const {messageAmountSummatory, increaseOrDecreaseMessageAmountSummatory} =
     useMessageAmountSummatory();
-  const {trigger: triggerMessageDeletion} = useDeleteMessage();
-  const {messagesWithTags, addMessageWithTags} = useMessagesWithTags();
+  const {messagesWithTags} = useMessagesWithTags();
   const {tags} = useTags();
-  const {createMessagesTagsTrigger} = useCreateMessagesTags();
   const {createTagsTrigger} = useCreateTags();
+  const {createMessageWithTagsTrigger} = useCreateMessageWithTags();
+  const {deleteMessageWithTagsTrigger} = useDeleteMessageWithTags();
 
   const onSendButtonPress = (message: string) => {
-    const isExpense = validateExpense(message);
-    const stringifiedAmount = findAmount(message);
     const tagNames = findTags(message);
-    const description = cleanMessageDescription(
-      message,
-      isExpense,
-      stringifiedAmount,
-      tagNames,
-    );
-    const amount = Number(stringifiedAmount) * (isExpense ? -1 : 1);
+    const creatableMessage = getCreatableMessageFromString(message, tagNames);
 
-    triggerMessageCreation(
-      {
-        amount,
-        description,
-        isExpense,
-      },
-      {
-        onSuccess(createdMessage) {
-          if (!createdMessage) {
-            return;
-          }
-          const alreadyCreatedTags = tags?.filter(tag =>
-            tagNames?.includes(tag.name),
-          );
-          createMessagesTagsTrigger(
-            (alreadyCreatedTags ?? []).map(tag => ({
-              messageId: createdMessage.id,
-              tagId: tag.id,
-            })),
-          );
-          const newTagNames = tagNames?.filter(
-            tagName => !tags?.map(tag => tag.name).includes(tagName),
-          );
-          createTagsTrigger(newTagNames ?? [], {
-            onSuccess(createdTags) {
-              createMessagesTagsTrigger(
-                (createdTags ?? []).map(tag => ({
-                  messageId: createdMessage.id,
-                  tagId: tag.id,
-                })),
-              );
-              if (!createdMessage || !createdTags || !alreadyCreatedTags) {
-                return;
-              }
-              addMessageWithTags(createdMessage, [
-                ...alreadyCreatedTags,
-                ...createdTags,
-              ]);
-            },
-          });
-        },
-      },
+    const newTagNames = tagNames?.filter(
+      tagName => !tags?.map(tag => tag.name).includes(tagName),
     );
-    increaseOrDecreaseMessageAmountSummatory(amount);
+    createTagsTrigger(newTagNames ?? [], {
+      onSuccess(createdTags) {
+        const alreadyCreatedTags = tags?.filter(tag =>
+          tagNames?.includes(tag.name),
+        );
+        createMessageWithTagsTrigger({
+          message: creatableMessage,
+          tags: [...(alreadyCreatedTags ?? []), ...(createdTags ?? [])],
+        });
+      },
+    });
+    increaseOrDecreaseMessageAmountSummatory(creatableMessage.amount);
+  };
+
+  const getOnDeletePressHandler = (message: MessageWithTags) => () => {
+    increaseOrDecreaseMessageAmountSummatory(message.amount * -1);
+    deleteMessageWithTagsTrigger({
+      messageId: message.id,
+      tags: message.tags,
+    });
   };
 
   return (
@@ -113,10 +81,7 @@ export const ChatScreen = () => {
               {
                 title: LOCALE.common.delete,
                 iconName: 'delete',
-                onPress: () => {
-                  triggerMessageDeletion(message.id);
-                  increaseOrDecreaseMessageAmountSummatory(message.amount * -1);
-                },
+                onPress: getOnDeletePressHandler(message),
               },
             ]}
             twClass="m-2"
