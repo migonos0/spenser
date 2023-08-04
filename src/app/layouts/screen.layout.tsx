47e91dc28a9096555639c9153bcade1476591a12
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-04 09:44:42 -0500

    Removed appbar and appbar props, due to the appbar component now being render by the stack navigator.

--- src/app/layouts/screen.layout.tsx
+++ src/app/layouts/screen.layout.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames';
 import {ReactNode} from 'react';
 import {Surface} from 'react-native-paper';
-import {Appbar, AppbarProps} from '../components/app-bar';
 import {View} from 'react-native';
 import {Colors} from '../../types/colors';
 
@@ -10,13 +9,10 @@ interface ScreenLayoutProps {
   twClass?: string;
   colors: Colors;
   footer?: ReactNode;
-  appbar: AppbarProps;
 }
 
 export const ScreenLayout = (props: ScreenLayoutProps) => (
   <Surface className={classNames('h-full flex', props.twClass)}>
-    <Appbar {...props.appbar} />
-
     <View className="flex-1">{props.children}</View>
 
     <View>{props.footer}</View>
