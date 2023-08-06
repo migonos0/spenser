fb6023e438481307b0b227b956c522a386e06261
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-06 17:03:26 -0500

    Installed babel-plugin-module-resolver and set up module-resolver under babel.config.ts

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['nativewind/babel'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        alias: {
+          'react-native-sqlite-storage': 'react-native-quick-sqlite',
+        },
+      },
+    ],
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
