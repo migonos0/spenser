09a33faf872befecfd8418f92e0690d17563e302
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-05 21:20:58 -0500

    Renamed usages of useSWRSqlite hook to useSQRImmutableSQLite. Implemented useMessagesByTagId data hook. Added mutate statement to properly add the newly created messages with tags to their according state of MessagesByTag.

--- src/state/message.state.ts
+++ src/state/message.state.ts
@@ -1,9 +1,10 @@
 import {useCallback} from 'react';
-import {TAGS_TABLE_NAME} from '../constants/db';
-import {useSWRSQLite, useSWRSQLiteMutation} from '../hooks/use-swr';
+import {MESSAGES_TABLE_NAME, TAGS_TABLE_NAME} from '../constants/db';
+import {useSWRImmutableSQLite, useSWRSQLiteMutation} from '../hooks/use-swr';
 import {
   createMessageWithTags,
   deleteMessageWithTagsById,
+  findAllMessagesByTagId,
   findAllMessagesWithTags,
   findMessageAmountSummatory,
 } from '../services/message.service';
@@ -11,12 +12,12 @@ import {
   MESSAGES_WITH_TAGS_KEY,
   MESSAGE_AMOUNT_SUMMATORY_KEY,
 } from '../constants/swr-keys';
-import {MessageWithTags} from '../schemas/message.schema';
+import {Message, MessageWithTags} from '../schemas/message.schema';
 import {useSWRConfig} from 'swr';
 import {Tag} from '../schemas/tag.schema';
 
 export const useMessageAmountSummatory = () => {
-  const {data, mutate} = useSWRSQLite(
+  const {data, mutate} = useSWRImmutableSQLite(
     MESSAGE_AMOUNT_SUMMATORY_KEY,
     findMessageAmountSummatory,
   );
@@ -37,12 +38,13 @@ export const useMessageAmountSummatory = () => {
 export const useMessagesWithTags = (params?: {ascendant?: boolean}) => {
   const fetcher = findAllMessagesWithTags({ascendant: params?.ascendant});
 
-  const {data} = useSWRSQLite(MESSAGES_WITH_TAGS_KEY, fetcher);
+  const {data} = useSWRImmutableSQLite(MESSAGES_WITH_TAGS_KEY, fetcher);
 
   return {messagesWithTags: data};
 };
 
 export const useCreateMessageWithTags = (params?: {ascendant?: boolean}) => {
+  const {mutate} = useSWRConfig();
   const {trigger} = useSWRSQLiteMutation(
     MESSAGES_WITH_TAGS_KEY,
     createMessageWithTags,
@@ -50,6 +52,18 @@ export const useCreateMessageWithTags = (params?: {ascendant?: boolean}) => {
       if (!result) {
         return currentData;
       }
+      /**
+       * Adding created message to MessagesByTag state
+       */
+      for (const tag of result.tags) {
+        mutate(
+          [MESSAGES_TABLE_NAME, tag.id],
+          (messages: Message[] | undefined) => {
+            return [...(messages ?? []), ...[{...result, tags: undefined}]];
+          },
+        );
+      }
+
       return params?.ascendant
         ? [...(currentData ?? []), ...[result]]
         : [...[result], ...(currentData ?? [])];
@@ -84,4 +98,13 @@ export const useDeleteMessageWithTags = () => {
   );
 
   return {deleteMessageWithTagsTrigger: trigger};
+};
+
+export const useMessagesByTagId = (tagId?: Tag['id']) => {
+  const key = tagId ? [MESSAGES_TABLE_NAME, tagId] : undefined;
+  const fetcher = tagId ? findAllMessagesByTagId(tagId) : () => undefined;
+
+  const {data} = useSWRImmutableSQLite(key, fetcher);
+
+  return {messages: data};
 };
