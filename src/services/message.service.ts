2bfedb638eb01b159aef71efcc33a31bb6113bce
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-13 20:28:07 -0500

    Modified message and tag services to use typeorm instead of raw sqlite queries.

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -1,174 +1,49 @@
-import {SQLiteDatabase} from 'react-native-sqlite-storage';
-import {MESSAGES_TABLE_NAME} from '../constants/db';
-import {Message} from '../schemas/message.schema';
-import {getInsertId, validateRowAffectation} from '../lib/sqlite';
 import {number} from 'zod';
-import {
-  createMessagesTags,
-  deleteMessagesTagsByMessageId,
-  findAllMessagesTagsByMessageId,
-  findAllMessagesTagsByTagId,
-} from './message-tag.service';
-import {Tag} from '../schemas/tag.schema';
-import {deleteTagById, findTagById} from './tag.service';
-
-export const createMessagesTable = async (db: SQLiteDatabase) => {
-  const query = `CREATE TABLE IF NOT EXISTS ${MESSAGES_TABLE_NAME}(
-        is_expense INTEGER NOT NULL,
-        amount NUMERIC NOT NULL,
-        description TEXT NOT NULL
-    );`;
-
-  await db.executeSql(query);
-};
+import {DataSource} from 'typeorm';
+import {Message} from '../entities/message';
 
 export const findAllMessages =
   ({ascendant}: {ascendant?: boolean}) =>
-  async (db: SQLiteDatabase) => {
-    const messages: Message[] = [];
-    const results = await db.executeSql(
-      `SELECT rowid as id, is_expense as isExpense, amount, description FROM ${MESSAGES_TABLE_NAME}${
-        ascendant ? ' ORDER BY id asc' : ' ORDER BY id desc'
-      }`,
-    );
-    for (const result of results) {
-      for (let index = 0; index < result.rows.length; index++) {
-        messages.push(result.rows.item(index));
-      }
-    }
-    return messages;
-  };
+  async (dataSource: DataSource) =>
+    await dataSource.manager.find(Message, {
+      order: {id: ascendant ? 'ASC' : 'DESC'},
+      relations: {tags: true},
+    });
 
 export const createMessage =
-  (message: Omit<Message, 'id'>) => async (db: SQLiteDatabase) => {
-    const insertQuery = `INSERT OR REPLACE INTO 
-    ${MESSAGES_TABLE_NAME}(is_expense, amount, description) values (
-    ${+message.isExpense}, 
-    ${message.amount}, 
-    '${message.description}')`;
+  (message: Message) => async (dataSource: DataSource) =>
+    await dataSource.manager.save(message);
 
-    const result = await db.executeSql(insertQuery);
+export const deleteMessageById =
+  (messageId: Message['id']) => async (dataSource: DataSource) => {
+    const foundMessage = await dataSource.manager.findOne(Message, {
+      relations: {tags: true},
+      where: {id: messageId},
+    });
 
-    return {...message, id: getInsertId(result)};
+    if (!foundMessage) {
+      return;
+    }
+
+    return await dataSource.manager.remove(foundMessage);
   };
 
-export const deleteMessageById = (id: number) => async (db: SQLiteDatabase) => {
-  const deleteQuery = `DELETE from ${MESSAGES_TABLE_NAME} where rowid = ${id}`;
-  const response = await db.executeSql(deleteQuery);
-  validateRowAffectation(response);
+export const findMessageAmountSummatory = async (dataSource: DataSource) => {
+  const {sum} = await dataSource
+    .getRepository(Message)
+    .createQueryBuilder('message')
+    .select('SUM(message.amount)', 'sum')
+    .getRawOne();
 
-  return id;
-};
-
-export const dropMessagesTable = async (db: SQLiteDatabase) => {
-  const query = `DROP TABLE ${MESSAGES_TABLE_NAME}`;
-
-  await db.executeSql(query);
-};
-
-export const findMessageAmountSummatory = async (db: SQLiteDatabase) => {
-  const query = `SELECT SUM(amount) as messageAmountSummatory FROM ${MESSAGES_TABLE_NAME}`;
-
-  const results = await db.executeSql(query);
-
-  const parsedMessageAmountSummatory = number().safeParse(
-    results[0].rows.item(0).messageAmountSummatory,
-  );
+  const parsedMessageAmountSummatory = number().safeParse(sum);
 
   if (!parsedMessageAmountSummatory.success) {
-    return 0;
+    return;
   }
 
   return parsedMessageAmountSummatory.data;
 };
 
-export const findAllMessagesWithTags =
-  ({ascendant}: {ascendant?: boolean}) =>
-  async (db: SQLiteDatabase) => {
-    const messagesWithTags: (Message & {tags: Tag[]})[] = [];
-
-    const messages = await findAllMessages({ascendant: ascendant})(db);
-    for (const message of messages) {
-      const messageTagIds = (
-        await findAllMessagesTagsByMessageId(message.id)(db)
-      ).map(messageTag => messageTag.tagId);
-
-      const tags: Tag[] = [];
-      for (const messageTagId of messageTagIds) {
-        const foundTag = await findTagById(messageTagId)(db);
-        if (!foundTag) {
-          continue;
-        }
-        tags.push(foundTag);
-      }
-      messagesWithTags.push({...message, tags});
-    }
-
-    return messagesWithTags;
-  };
-
-export const createMessageWithTags =
-  (input: {message: Omit<Message, 'id'>; tags: Tag[]}) =>
-  async (db: SQLiteDatabase) => {
-    const createdMessage = await createMessage(input.message)(db);
-    await createMessagesTags(
-      input.tags.map(tag => ({tagId: tag.id, messageId: createdMessage.id})),
-    )(db);
-
-    return {...createdMessage, tags: input.tags};
-  };
-
-export const deleteMessageWithTagsById =
-  (input: {messageId: Message['id']; tags: Tag[]}) =>
-  async (db: SQLiteDatabase) => {
-    const deletedMessageId = await deleteMessageById(input.messageId)(db);
-    const deletedTagIds: Tag['id'][] = [];
-    for (const tag of input.tags) {
-      const foundMessagesTags = await findAllMessagesTagsByTagId(tag.id)(db);
-      if (foundMessagesTags.length > 0) {
-        continue;
-      }
-      const deletedTagId = await deleteTagById(tag.id)(db);
-      deletedTagIds.push(deletedTagId);
-    }
-
-    if (input.tags.length < 1) {
-      return {deletedMessageId, deletedTagIds};
-    }
-
-    await deleteMessagesTagsByMessageId(input.messageId)(db);
-    return {deletedMessageId, deletedTagIds};
-  };
-
 export const findMessageById =
-  (messageId: Message['id']) => async (db: SQLiteDatabase) => {
-    const messages: Message[] = [];
-    const results = await db.executeSql(
-      `SELECT rowid as id, is_expense as isExpense, amount, description FROM ${MESSAGES_TABLE_NAME} WHERE id = ${messageId}`,
-    );
-    for (const result of results) {
-      for (let index = 0; index < result.rows.length; index++) {
-        messages.push(result.rows.item(index));
-      }
-    }
-    return messages.shift();
-  };
-
-export const findAllMessagesByTagId =
-  (tagId: Tag['id']) => async (db: SQLiteDatabase) => {
-    const messageIds = (await findAllMessagesTagsByTagId(tagId)(db)).map(
-      messageTag => messageTag.messageId,
-    );
-
-    const messages: Message[] = [];
-
-    for (const messageId of messageIds) {
-      const foundMessage = await findMessageById(messageId)(db);
-      if (!foundMessage) {
-        continue;
-      }
-      messages.push(foundMessage);
-    }
-
-    return messages;
-  };
+  (messageId: Message['id']) => async (dataSource: DataSource) =>
+    await dataSource.manager.findOneBy(Message, {id: messageId});
--- src/services/tag.service.ts
+++ src/services/tag.service.ts
@@ -1,72 +1,23 @@
-import {SQLiteDatabase} from 'react-native-sqlite-storage';
-import {TAGS_TABLE_NAME} from '../constants/db';
-import {Tag} from '../schemas/tag.schema';
-import {getInsertId, validateRowAffectation} from '../lib/sqlite';
+import {DataSource} from 'typeorm';
+import {Tag} from '../entities/tag';
 
-export const createTagsTable = async (db: SQLiteDatabase) => {
-  const query = `CREATE TABLE IF NOT EXISTS ${TAGS_TABLE_NAME}(
-          name TEXT NOT NULL UNIQUE
-      );`;
+export const findAllTags = async (dataSource: DataSource) =>
+  await dataSource.manager.find(Tag);
 
-  await db.executeSql(query);
-};
-
-export const findAllTags = async (db: SQLiteDatabase) => {
-  const tags: Tag[] = [];
-  const results = await db.executeSql(
-    `SELECT rowid as id, name FROM ${TAGS_TABLE_NAME}`,
-  );
-  for (const result of results) {
-    for (let index = 0; index < result.rows.length; index++) {
-      tags.push(result.rows.item(index));
+export const deleteTagById =
+  (tagId: Tag['id']) => async (dataSource: DataSource) => {
+    const foundTag = await dataSource.manager.findOneBy(Tag, {id: tagId});
+    if (!foundTag) {
+      return;
     }
-  }
-  return tags;
-};
 
-export const createTags =
-  (tagNames: Tag['name'][]) =>
-  async (db: SQLiteDatabase): Promise<Tag[]> => {
-    if (tagNames.length < 1) {
-      return [];
-    }
-    const insertQuery = `INSERT OR REPLACE INTO 
-    ${TAGS_TABLE_NAME}(name) values ${tagNames
-      .map(tagName => `('${tagName}')`)
-      .join(',')}`;
-
-    const result = await db.executeSql(insertQuery);
-    const lastInsertedTagId = getInsertId(result);
-
-    return tagNames.map((tagName, index) => ({
-      id: lastInsertedTagId - tagNames.length + index + 1,
-      name: tagName,
-    }));
+    return await dataSource.manager.remove(foundTag);
   };
 
-export const deleteTagById = (id: number) => async (db: SQLiteDatabase) => {
-  const deleteQuery = `DELETE from ${TAGS_TABLE_NAME} where rowid = ${id}`;
-  const response = await db.executeSql(deleteQuery);
-  validateRowAffectation(response);
+export const findTagById =
+  (tagId: Tag['id']) => async (dataSource: DataSource) =>
+    await dataSource.manager.findOneBy(Tag, {id: tagId});
 
-  return id;
-};
-
-export const dropTagsTable = async (db: SQLiteDatabase) => {
-  const query = `DROP TABLE ${TAGS_TABLE_NAME}`;
-
-  await db.executeSql(query);
-};
-
-export const findTagById = (tagId: Tag['id']) => async (db: SQLiteDatabase) => {
-  const tags: Tag[] = [];
-  const results = await db.executeSql(
-    `SELECT rowid as id, name FROM ${TAGS_TABLE_NAME} WHERE id = ${tagId}`,
-  );
-  for (const result of results) {
-    for (let index = 0; index < result.rows.length; index++) {
-      tags.push(result.rows.item(index));
-    }
-  }
-  return tags.shift();
-};
+export const findMessagesByTagId =
+  (tagId: Tag['id']) => async (dataSource: DataSource) =>
+    (await dataSource.manager.findOneBy(Tag, {id: tagId}))?.messages;
