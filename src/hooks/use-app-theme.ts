040567d84555c7d0b1b87e13f924d6ec241b9035
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-15 11:35:01 -0500

    Modified useAppTheme hook typings.

--- src/hooks/use-app-theme.ts
+++ src/hooks/use-app-theme.ts
@@ -1,5 +1,3 @@
-import {useTheme} from 'react-native-paper';
+import {MD3Theme, useTheme} from 'react-native-paper';
 
-import {Material3Scheme} from '@pchmn/expo-material3-theme';
-
-export const useAppTheme = useTheme<Material3Scheme>;
+export const useAppTheme = useTheme<MD3Theme>;
