70a5a62600f06a7928e3f51421ad3834c07ba88d
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-15 14:05:42 -0500

    Declared createdAt an updatedAt entity fiels.

--- src/entities/tracker.ts
+++ src/entities/tracker.ts
@@ -9,15 +9,25 @@ export class Tracker {
   @Column()
   name: string;
 
+  @Column({type: 'date'})
+  createdAt: Date;
+
+  @Column({type: 'date'})
+  updatedAt: Date;
+
   @OneToMany(() => Message, message => message.tracker)
   messages?: Message[];
 
   constructor(
     name: Tracker['name'],
+    createdAt: Tracker['createdAt'] = new Date(),
+    updatedAt: Tracker['updatedAt'] = new Date(),
     messages: Tracker['messages'],
     id: Tracker['id'],
   ) {
     this.name = name;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
     this.messages = messages;
     this.id = id;
   }
