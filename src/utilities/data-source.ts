c7244ac1c86f104bf70256f3c2661193e3e656ca
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-15 10:33:05 -0500

    Added Tag and Message entities to data source

--- src/utilities/data-source.ts
+++ src/utilities/data-source.ts
@@ -3,12 +3,14 @@ import {DataSource} from 'typeorm';
 
 import {typeORMDriver} from 'react-native-quick-sqlite';
 import {DB_NAME} from '../constants/db';
+import {Tag} from '../entities/tag';
+import {Message} from '../entities/message';
 
 export const dataSource = new DataSource({
   type: 'react-native',
   database: DB_NAME,
   location: '.',
   driver: typeORMDriver,
-  entities: [],
+  entities: [Tag, Message],
   synchronize: true,
 });
