2ed8900a98b3a9e272eef095263199f73b23f638
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-17 11:07:40 -0500

    Implemented function to find the balance of a tracker based on its id. Modified findAllTrackers service function so it injects the balance to the found trackers.

--- src/services/tracker.service.ts
+++ src/services/tracker.service.ts
@@ -1,11 +1,36 @@
 import {DataSource} from 'typeorm';
 import {Tracker} from '../entities/tracker';
+import {Message} from '../entities/message';
+import {number, safeParse} from 'valibot';
 
 export const createTracker =
   (tracker: Tracker) => async (dataSource: DataSource) =>
     await dataSource.manager.save(tracker);
 
-export const findAllTrackers = async (dataSource: DataSource) =>
-  await dataSource.manager.find(Tracker, {
+export const findBalanceByTrackerId =
+  (trackerId: Tracker['id']) => async (dataSource: DataSource) => {
+    const {balance} = await dataSource
+      .createQueryBuilder()
+      .from(Message, 'message')
+      .select('SUM(message.amount)', 'balance')
+      .where('message.trackerId = :trackerId', {trackerId})
+      .getRawOne();
+
+    const parsedBalance = safeParse(number(), balance);
+
+    if (!parsedBalance.success) {
+      return;
+    }
+
+    return parsedBalance.data;
+  };
+
+export const findAllTrackers = async (dataSource: DataSource) => {
+  const trackers = await dataSource.manager.find(Tracker, {
     order: {updatedAt: {direction: 'DESC'}},
   });
+  for (const tracker of trackers) {
+    tracker.balance = await findBalanceByTrackerId(tracker.id)(dataSource);
+  }
+  return trackers;
+};
