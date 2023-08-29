59cb325f11e5ce39b582b1f59518b73534fc99b7
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-08-29 14:04:35 -0500

    Modified chat box component to work with external values instead of an internal state.

--- src/app/components/chat-box.tsx
+++ src/app/components/chat-box.tsx
@@ -1,5 +1,4 @@
-import {useState} from 'react';
-import {View} from 'react-native';
+import {TextInputProps, View} from 'react-native';
 import {Button, TextInput} from 'react-native-paper';
 
 import {LOCALE} from '../../constants/locale';
@@ -7,30 +6,26 @@ import {cn} from '../../utilities/cn';
 
 interface ChatBoxProps {
   twClass?: string;
-  onSendButtonPress?: (t: string) => void;
+  onSendButtonPress?: () => void;
   avoidCleaningAfterSent?: boolean;
+  value?: TextInputProps['value'];
+  onChange?: TextInputProps['onChangeText'];
+  onBlur?: TextInputProps['onBlur'];
 }
 export const ChatBox = ({twClass, ...props}: ChatBoxProps) => {
-  const [text, setText] = useState<string>('');
-
   return (
     <View className={cn('flex flex-row w-full px-5 py-3', twClass)}>
       <View className="flex-1 flex justify-center content-center pb-2 mr-2">
         <TextInput
+          onBlur={props.onBlur}
           multiline
-          value={text}
-          onChangeText={t => setText(t)}
+          value={props.value}
+          onChangeText={props.onChange}
           mode="outlined"
         />
       </View>
       <View className="flex justify-center">
-        <Button
-          onPress={() => {
-            props.onSendButtonPress && props.onSendButtonPress(text);
-            !props.avoidCleaningAfterSent && setText('');
-          }}
-          icon="send"
-          mode="contained">
+        <Button onPress={props.onSendButtonPress} icon="send" mode="contained">
           {LOCALE.common.send}
         </Button>
       </View>
