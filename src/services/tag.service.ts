6eacb9b5f5f08322b999c34265552517e561a14b
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-10-02 22:13:08 -0500

    Removed the data source parameter from every service function.

--- src/services/tag.service.ts
+++ src/services/tag.service.ts
@@ -1,45 +1,42 @@
-import {DataSource} from 'typeorm';
-
 import {Tag} from '../entities/tag';
 import {Account} from '../entities/account';
 import {Transaction} from '../entities/transaction';
+import {dataSource} from '../utilities/data-source';
 
-export const findAllTags = async (dataSource: DataSource) =>
-  await dataSource.manager.find(Tag);
+export const findAllTags = async () => await dataSource.manager.find(Tag);
 
-export const deleteTagById =
-  (tagId: Tag['id']) => async (dataSource: DataSource) => {
-    const foundTag = await dataSource.manager.findOneBy(Tag, {id: tagId});
-    if (!foundTag) {
-      return;
-    }
+export const deleteTagById = async (tagId: Tag['id']) => {
+  const foundTag = await dataSource.manager.findOneBy(Tag, {id: tagId});
+  if (!foundTag) {
+    return;
+  }
 
-    return await dataSource.manager.remove(foundTag);
-  };
+  return await dataSource.manager.remove(foundTag);
+};
 
-export const findTagById =
-  (tagId: Tag['id']) => async (dataSource: DataSource) =>
-    await dataSource.manager.findOneBy(Tag, {id: tagId});
+export const findTagById = async (tagId: Tag['id']) =>
+  await dataSource.manager.findOneBy(Tag, {id: tagId});
 
-export const findTransactionsByAccountAndTagIds =
-  (accountId: Account['id'], tagId: Tag['id']) => async (ds: DataSource) =>
-    await ds
-      .getRepository(Transaction)
-      .createQueryBuilder('transaction')
-      .innerJoin('transaction.tags', 'tag')
-      .where('transaction.accountId = :accountId', {accountId})
-      .andWhere('tag.id = tagId', {tagId})
-      .orderBy('transaction.id', 'DESC')
-      .getMany();
+export const findTransactionsByAccountAndTagIds = async (
+  accountId: Account['id'],
+  tagId: Tag['id'],
+) =>
+  await dataSource
+    .getRepository(Transaction)
+    .createQueryBuilder('transaction')
+    .innerJoin('transaction.tags', 'tag')
+    .where('transaction.accountId = :accountId', {accountId})
+    .andWhere('tag.id = tagId', {tagId})
+    .orderBy('transaction.id', 'DESC')
+    .getMany();
 
-export const createTag = (tag: Tag) => async (dataSource: DataSource) =>
-  await dataSource.manager.save(tag);
+export const createTag = async (tag: Tag) => await dataSource.manager.save(tag);
 
-export const createTags = (tags: Tag[]) => async (dataSource: DataSource) => {
+export const createTags = async (tags: Tag[]) => {
   const createdTags: Tag[] = [];
 
   for (const tag of tags) {
-    createdTags.push(await createTag(tag)(dataSource));
+    createdTags.push(await createTag(tag));
   }
 
   return createdTags;
