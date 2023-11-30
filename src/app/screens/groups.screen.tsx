8de1415b530b318f1dc04eec1e72f50642f37558
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-11-30 14:52:02 -0500

    Created base groups screen

---
+++ src/app/screens/groups.screen.tsx
@@ -0,0 +1,10 @@
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+export const GroupsScreen = () => {
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  );
+};
