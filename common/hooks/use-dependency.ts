6bc04f9303694a6fb294c22383d064ef4d98bb3a
Miguel Rodríguez <miguel_rodriguez@disroot.org>
2024-09-29 10:02:28 -0500

    Minor enhancements to the handling of dependencies.c

--- common/hooks/use-dependency.ts
+++ common/hooks/use-dependency.ts
@@ -3,33 +3,33 @@ import {useDepsContext} from './use-deps-context';
 import {DepNames} from '@/common/infra/deps-container';
 
 export const useDependency = <T = unknown>(dependencyName: DepNames): T => {
-  const {depsContainer, version: containerVersion} = useDepsContext();
-  const dependencyVersion = useRef<number>();
-  const cachedDependency = useRef<T>();
+    const {depsContainer, version: containerVersion} = useDepsContext();
+    const dependencyVersion = useRef<number>();
+    const cachedDependency = useRef<T>();
 
-  const dependency = useMemo(() => {
-    if (dependencyVersion.current === undefined) {
-      const freshDependency = depsContainer.resolve(dependencyName);
-      dependencyVersion.current = 0;
-      cachedDependency.current = freshDependency;
-      return freshDependency;
+    const dependency = useMemo(() => {
+        if (dependencyVersion.current === undefined) {
+            const freshDependency = depsContainer.resolve(dependencyName);
+            dependencyVersion.current = 0;
+            cachedDependency.current = freshDependency;
+            return freshDependency;
+        }
+
+        if (dependencyVersion.current === containerVersion) {
+            return cachedDependency.current;
+        }
+
+        const freshDependency = depsContainer.resolve(dependencyName);
+        dependencyVersion.current = dependencyVersion.current + 1;
+        cachedDependency.current = freshDependency;
+        return freshDependency;
+    }, [depsContainer, dependencyName, containerVersion]);
+
+    if (!dependency) {
+        throw new Error(
+            `Dependency is not available.\n{"${dependencyName}": ${dependency}}`,
+        );
     }
 
-    if (dependencyVersion.current === containerVersion) {
-      return cachedDependency.current;
-    }
-
-    const freshDependency = depsContainer.resolve(dependencyName);
-    dependencyVersion.current = dependencyVersion.current + 1;
-    cachedDependency.current = freshDependency;
-    return freshDependency;
-  }, [depsContainer, dependencyName, containerVersion]);
-
-  if (!dependency) {
-    throw new Error(
-      `Dependency is not available.\n{${dependencyName}: ${dependency}}`,
-    );
-  }
-
-  return dependency;
+    return dependency;
 };
